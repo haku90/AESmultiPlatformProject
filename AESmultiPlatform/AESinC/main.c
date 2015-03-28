5554704348f812b4e24a0dd3786e338f63e60e41
Adam Hakowski <adam.hakowski@gmail.com>
2015-03-28 11:01:26 +0100

    TesteFileReader

--- AESmultiPlatform/AESinC/main.c
+++ AESmultiPlatform/AESinC/main.c
@@ -1,4 +1,22 @@
+#include<stdio.h>
+
 void main()
 {
+	FILE *ptr_file;
+	char buf[1000];
+
+	ptr_file = fopen("message.txt", "r");
+	if (!ptr_file)
+		return;
+
+	while (fgets(buf, 1000, ptr_file) != NULL)
+	{
+		printf("%s", buf);
+	}
+	printf("%c\n", buf[2]);
+	char temp = buf[2];
+	printf("%c\n", buf[4]);
+
+	fclose(ptr_file);
 
 }
