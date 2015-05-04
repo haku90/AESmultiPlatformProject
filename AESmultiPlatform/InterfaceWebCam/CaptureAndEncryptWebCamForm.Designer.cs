d6a43c7eb986e7ec0b91ebe8b8da6814a658ccc3
Adam Hakowski <adam.hakowski@gmail.com>
2015-05-04 21:02:46 +0200

    Add CaptureWebCam

---
+++ AESmultiPlatform/InterfaceWebCam/CaptureAndEncryptWebCamForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace InterfaceWebCam
+{
+    partial class uxKey
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControl11 = new WebCamCapture.UserControl1();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.uxKeyEncrypt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.uxKeyForDecrypt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // userControl11
+            // 
+            this.userControl11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.userControl11.Location = new System.Drawing.Point(12, 12);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(260, 179);
+            this.userControl11.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(38, 210);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 38);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(167, 210);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 38);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Koniec";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // uxKeyEncrypt
+            // 
+            this.uxKeyEncrypt.Location = new System.Drawing.Point(402, 22);
+            this.uxKeyEncrypt.Name = "uxKeyEncrypt";
+            this.uxKeyEncrypt.Size = new System.Drawing.Size(232, 20);
+            this.uxKeyEncrypt.TabIndex = 3;
+            this.uxKeyEncrypt.TextChanged += new System.EventHandler(this.uxKeyEncrypt_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(360, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Klucz:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Klucz do odszyfrowania:";
+            // 
+            // uxKeyForDecrypt
+            // 
+            this.uxKeyForDecrypt.Location = new System.Drawing.Point(402, 57);
+            this.uxKeyForDecrypt.Name = "uxKeyForDecrypt";
+            this.uxKeyForDecrypt.Size = new System.Drawing.Size(232, 20);
+            this.uxKeyForDecrypt.TabIndex = 6;
+            this.uxKeyForDecrypt.TextChanged += new System.EventHandler(this.uxKeyForDecrypt_TextChanged);
+            // 
+            // uxKey
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 262);
+            this.Controls.Add(this.uxKeyForDecrypt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uxKeyEncrypt);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.userControl11);
+            this.Name = "uxKey";
+            this.Text = "Szyfrowanie Obrazu z Kamery";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private WebCamCapture.UserControl1 userControl11;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox uxKeyEncrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox uxKeyForDecrypt;
+    }
+}
+
---
+++ AESmultiPlatform/InterfaceWebCam/CaptureAndEncryptWebCamForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InterfaceWebCam
+{
+    public partial class uxKey : Form
+    {
+        public uxKey()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            userControl11.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            userControl11.Stop();
+        }
+
+        private void uxKeyEncrypt_TextChanged(object sender, EventArgs e)
+        {
+            userControl11.Stop();
+            userControl11.Key = uxKeyEncrypt.Text;
+            
+        }
+
+        private void uxKeyForDecrypt_TextChanged(object sender, EventArgs e)
+        {
+            userControl11.Stop();
+            userControl11.KeyForDecrypt = uxKeyForDecrypt.Text;
+            
+        }
+    }
+}
